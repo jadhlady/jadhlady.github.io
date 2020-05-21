88a9507f8cd12de22106307ad2d9be6b7ea063f1
jadhlady <56136691+jadhlady@users.noreply.github.com>
2020-05-21 05:18:39 -0600

    added blackjack game

--- slideshow.js
+++ slideshow.js
@@ -1,4 +1,4 @@
-var slideIndex = [1,1];
+var slideIndex = [1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
 var slideId = ["mySlides1", "mySlides2", "mySlides3"]
 showSlides(1, 0);
